424906c158d8054cdef9b37e0b37593f42312085
clabornd <dmclaborne@gmail.com>
2024-08-04 18:33:32 -0700

    timestep sinusoid function

---
+++ stable_diffusion/timestep.py
@@ -0,0 +1,16 @@
+import torch
+
+def time_embeddings(t, out_dim, max_period=10000):
+    half = out_dim // 2
+    denom = torch.exp(-torch.tensor(max_period).log()*torch.arange(0, half, dtype=torch.float32)/half)
+    phases = t[:, None] * denom[None]
+
+    # concatentate to form a tensor of shape B x out_dim
+    out_emb = torch.cat([phases.sin(), phases.cos()], dim = -1)
+    
+    # if out_dim is odd, add a zero column
+    if out_dim % 2:
+        out_emb = torch.cat([out_emb, torch.zeros_like(out_emb[:, :1])], dim = -1)
+
+    return out_emb
+
